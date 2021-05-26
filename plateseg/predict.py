be856abae2a4c98a29c07783f91286ee3f35e198
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2021-05-26 16:18:00 +1000

    Make importable, write generic make_chunks func.

--- plateseg/predict.py
+++ plateseg/predict.py
@@ -9,16 +9,12 @@ import napari
 from napari.qt import thread_worker
 from skimage.exposure import rescale_intensity
 
-import unet
-import watershed as ws
+from . import unet
+from . import watershed as ws
 
 
 u_state_fn = os.path.join(
         os.path.dirname(__file__), 'data/unet-210525-zyxmc.pt'
-        )
-#data_fn = '/data/platelets/200519_IVMTR69_Inj4_dmso_exp3.nd2'
-data_fn = os.path.expanduser(
-        '~/Dropbox/share-files/200519_IVMTR69_Inj4_dmso_exp3.nd2'
         )
 
 u = unet.UNet(in_channels=1, out_channels=5)
@@ -30,27 +26,31 @@ if torch.cuda.is_available() and not IGNORE_CUDA:
 u.load_state_dict(
         torch.load(u_state_fn, map_location=torch.device(map_location))
         )
-layer_list = nd2.nd2_reader.nd2_reader(data_fn)
 
+def make_chunks(arr_shape, chunk_shape, margin):
+    ndim = len(arr_shape)
+    if type(margin) == int:
+        margin = [margin] * ndim
+    starts = []
+    crops = []
+    for dim in range(ndim):
+        arr = arr_shape[dim]
+        chk = chunk_shape[dim]
+        mrg = margin[dim]
+        start = np.arange(0, arr - chk + 2*mrg + 1, chk - 2*mrg)
+        start[-1] = arr - chk
+        if len(start) > 1 and start[-1] == start[-2]:
+            start = start[:-1]  # remove duplicates in case last step is perfect
+        starts.append(start)
+        crop = np.array([(mrg, chk - mrg),] * len(start))
+        crop[0, 0] = 0
+        crop[-1, 0] = chk - (arr - np.sum(crop[:-1, 1] - crop[:-1, 0]))
+        crop[-1, 1] = chk
+        crops.append(crop)
+    chunk_starts = list(itertools.product(*starts))
+    chunk_crops = list(itertools.product(*crops))
+    return chunk_starts, chunk_crops
 
-
-t_idx = 114
-
-source_vol = layer_list[2][0]
-vol2predict = rescale_intensity(
-        np.asarray(source_vol[t_idx])
-        ).astype(np.float32)
-prediction_output = np.zeros((5,) + vol2predict.shape, dtype=np.float32)
-
-chunk_start_0 = (0, 6, 12, 18, 23)
-chunk_start_1 = (0, 128, 256)
-chunk_start_2 = (0, 128, 256)
-crops_0 = [(0, 8), (2, 8), (2, 8), (2, 8), (3, 10)]
-crops_1 = [(0, 192), (64, 192), (64, 256)]
-crops_2 = [(0, 192), (64, 192), (64, 256)]
-chunk_starts = list(itertools.product(chunk_start_0, chunk_start_1, chunk_start_2))
-chunk_crops = list(itertools.product(crops_0, crops_1, crops_2))
-size = (10, 256, 256)
 
 def predict_output_chunks(
     unet, input_volume, chunk_size, chunk_starts, chunk_crops, output_volume
@@ -70,73 +70,92 @@ def predict_output_chunks(
         yield
     return output_volume
 
-viewer = napari.Viewer(ndisplay=3)
-l0 = viewer._add_layer_from_data(*layer_list[0])[0]
-l1 = viewer._add_layer_from_data(*layer_list[1])[0]
-l2 = viewer._add_layer_from_data(*layer_list[2])[0]
 
-offsets = -0.5 * np.asarray(l0.scale)[-3:] * np.eye(5, 3)
-prediction_layers = viewer.add_image(
-        prediction_output,
-        channel_axis=0,
-        name=['z-aff', 'y-aff', 'x-aff', 'mask', 'centroids'],
-        scale=l0.scale[-3:],
-        translate=list(np.asarray(l0.translate[-3:]) + offsets),
-        colormap=['bop purple', 'bop orange', 'bop orange', 'gray', 'gray'],
-        visible=[False, False, False, True, False],
+if __name__ == '__main__':
+    #data_fn = '/data/platelets/200519_IVMTR69_Inj4_dmso_exp3.nd2'
+    data_fn = os.path.expanduser(
+            '~/Dropbox/share-files/200519_IVMTR69_Inj4_dmso_exp3.nd2'
+            )
+    layer_list = nd2.nd2_reader.nd2_reader(data_fn)
+
+    t_idx = 114
+
+    source_vol = layer_list[2][0]
+    vol2predict = rescale_intensity(
+            np.asarray(source_vol[t_idx])
+            ).astype(np.float32)
+    prediction_output = np.zeros((5,) + vol2predict.shape, dtype=np.float32)
+
+    size = (10, 256, 256)
+    chunk_starts, chunk_crops = make_chunks(vol2predict.shape, size, (1, 0, 0))
+
+    viewer = napari.Viewer(ndisplay=3)
+    l0 = viewer._add_layer_from_data(*layer_list[0])[0]
+    l1 = viewer._add_layer_from_data(*layer_list[1])[0]
+    l2 = viewer._add_layer_from_data(*layer_list[2])[0]
+
+    offsets = -0.5 * np.asarray(l0.scale)[-3:] * np.eye(5, 3)
+    prediction_layers = viewer.add_image(
+            prediction_output,
+            channel_axis=0,
+            name=['z-aff', 'y-aff', 'x-aff', 'mask', 'centroids'],
+            scale=l0.scale[-3:],
+            translate=list(np.asarray(l0.translate[-3:]) + offsets),
+            colormap=['bop purple', 'bop orange', 'bop orange', 'gray', 'gray'],
+            visible=[False, False, False, True, False],
+            )
+    viewer.dims.set_point(0, t_idx)
+
+
+    def refresh_prediction_layers():
+        for layer in prediction_layers:
+            layer.refresh()
+
+
+    labels = np.pad(
+        np.zeros(prediction_output.shape[1:], dtype=np.uint32),
+        1,
+        mode='constant',
+        constant_values=0,
+    )
+    labels_layer = viewer.add_labels(
+            labels[1:-1, 1:-1, 1:-1],
+            name='watershed',
+            scale=prediction_layers[-1].scale,
+            translate=prediction_layers[-1].translate,
+            )
+
+    counter = 0
+    def refresh_labels():
+        # we throttle labels refreshes because they take a long time
+        global counter
+        counter += 1
+        if counter % 10000 == 1:
+            labels_layer.refresh()
+
+
+    # closure to connect to threadworker signal
+    def segment(prediction):
+        yield from ws.segment_output_image(
+            prediction,
+            affinities_channels=(0, 1, 2),
+            centroids_channel=4,
+            thresholding_channel=3,
+            out=labels.ravel()
         )
-viewer.dims.set_point(0, t_idx)
 
-
-def refresh_prediction_layers():
-    for layer in prediction_layers:
-        layer.refresh()
-
-
-labels = np.pad(
-    np.zeros(prediction_output.shape[1:], dtype=np.uint32),
-    1,
-    mode='constant',
-    constant_values=0,
-)
-labels_layer = viewer.add_labels(
-        labels[1:-1, 1:-1, 1:-1],
-        name='watershed',
-        scale=prediction_layers[-1].scale,
-        translate=prediction_layers[-1].translate,
-        )
-
-counter = 0
-def refresh_labels():
-    # we throttle labels refreshes because they take a long time
-    global counter
-    counter += 1
-    if counter % 10000 == 1:
-        labels_layer.refresh()
-
-
-# closure to connect to threadworker signal
-def segment(prediction):
-    yield from ws.segment_output_image(
-        prediction,
-        affinities_channels=(0, 1, 2),
-        centroids_channel=4,
-        thresholding_channel=3,
-        out=labels.ravel()
+    segment_worker = thread_worker(
+        segment,
+        connect={'yielded': refresh_labels}
     )
 
-segment_worker = thread_worker(
-    segment,
-    connect={'yielded': refresh_labels}
-)
+    prediction_worker = thread_worker(
+        predict_output_chunks,
+        connect={
+            'yielded': refresh_prediction_layers,
+            'returned': segment_worker
+            },
+    )
+    prediction_worker(u, vol2predict, size, chunk_starts, chunk_crops, prediction_output)
 
-prediction_worker = thread_worker(
-    predict_output_chunks,
-    connect={
-        'yielded': refresh_prediction_layers,
-        'returned': segment_worker
-        },
-)
-prediction_worker(u, vol2predict, size, chunk_starts, chunk_crops, prediction_output)
-
-napari.run()
+    napari.run()
