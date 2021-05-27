28d81de0aa88fdb61154cb06c2a5046bb7265ac1
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2021-05-27 05:04:00 -0500

    Remove example widgets

--- plateseg/_dock_widget.py
+++ plateseg/_dock_widget.py
@@ -183,31 +183,7 @@ class UNetPredictWidget(widgets.Container):
         self.call_watershed.enabled = True
 
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
-
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
-
-
-@magic_factory
-def example_magic_widget(img_layer: napari.layers.Image):
-    print(f"you have selected {img_layer}")
-
-
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
     # you can return either a single widget, or a sequence of widgets
-    return [UNetPredictWidget, ExampleQWidget]
+    return [UNetPredictWidget]
